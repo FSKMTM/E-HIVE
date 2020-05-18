967dabed5612c1419e56d9921ccd962adf4c9771
klogar <kl2164@student.uni-lj.si>
2020-05-18 11:03:27 +0200

    deluje iskanje brez datumov

--- frontend/src/views/Measurements.vue
+++ frontend/src/views/Measurements.vue
@@ -1,13 +1,37 @@
 <template>
 	<div>
 		<h3>Vse meritve</h3>
-        <div class="col col-sm-12 offset-sm-0 col-md-6 offset-md-3">
-            <SelectPodnica
+        <div class="col col-sm-12 offset-sm-0 col-md-10 offset-md-1">
+            <!-- <SelectPodnica
                 v-if="podnice.length"
                 @izbrana-podnica="izbranaPodnica"
                 :podnice="podnice"
             >
-            </SelectPodnica>
+            </SelectPodnica> -->
+            <b-form @submit.prevent="search" >
+
+                <b-form-group id="input-group-podnica" label="Izberi iskano podnico:" label-for="podnica">
+                    <b-form-select
+											v-show="podnice.length"
+											v-model="form.indeksIzbranePodnice"
+											:options="podnice"
+											value-field="id"
+											text-field="naziv"
+											>
+									</b-form-select>
+                </b-form-group>
+
+                <b-form-group id="input-group-tip" v-if="tipi.length" label="Izberi tip meritve:">
+                    <b-form-checkbox-group v-model="form.checked" id="checkboxes-tip">
+											<b-form-checkbox
+											v-for="tip in tipi"
+											:key="tip.koda"
+											:value="tip.koda">{{ tip.ime }}</b-form-checkbox>
+                    </b-form-checkbox-group>
+                </b-form-group>
+
+                <b-button type="submit" variant="primary">Poišči meritve</b-button>
+                </b-form>
         </div>
         <b-container class="actual-container">
             <b-row>
@@ -38,23 +62,23 @@
 <script>
 import global from '@/global'
 import moment from 'moment'
-import SelectPodnica from '@/components/SelectPodnica'
 
 export default {
-	components: {
-    SelectPodnica
-  },
 	props: {
 		podnice: Array
 	},
 	data () {
     return {
-            meritve: [],
-            limit: 10,
-            offset: 0,
-            naloziVec: true,
-            indeksIzbranePodnice: -1,
-            totalCount: 100,
+			meritve: [],
+			tipi: [],
+			limit: 10,
+			offset: 0,
+			naloziVec: true,
+			totalCount: 100,
+			form: {
+          indeksIzbranePodnice: -1,
+          checked: []
+        },
 			fields: [
 				{
 					key: "tip.koda",
@@ -79,20 +103,21 @@ export default {
   },
 	methods: {
 		pridobiMeritve: function () {
-			fetch(global.restIp + "/meritve?filter=podnica.id:EQ:" + this.indeksIzbranePodnice + "&order=cas_meritve DESC&limit=" + this.limit + "&offset=" + this.offset, {
+			console.log(global.restIp + "/meritve?" + this.iskalniNiz() + "&limit=" + this.limit + "&offset=" + this.offset + "&order=cas_meritve DESC")
+			fetch(global.restIp + "/meritve?" + this.iskalniNiz() + "&limit=" + this.limit + "&offset=" + this.offset + "&order=cas_meritve DESC", {
 				method: "get"
 			})
 				.then((response) => {
-                    this.totalCount = parseInt(response.headers.get('x-total-count'))
+          this.totalCount = parseInt(response.headers.get('x-total-count'))
 					return response.json()
 				})
 				.then((jsonData) => {
-                    console.log(jsonData)
-                    this.offset += this.limit
-                    this.meritve.push.apply(this.meritve, jsonData)
-                    if (this.meritve.length === this.totalCount) {
-                        this.naloziVec = false
-                    }
+					console.log(jsonData)
+					this.offset += this.limit
+					this.meritve.push.apply(this.meritve, jsonData)
+					if (this.meritve.length === this.totalCount) {
+							this.naloziVec = false
+					}
 				})
 		},
 		pridobiImeMeritve: function(koda) {
@@ -106,15 +131,40 @@ export default {
 				return "Notranja vlaga"
 			} else if (koda === "TEZA") {
 				return "Teža"
+			} else if (koda === "VAROA") {
+				return "Varoa"
 			}
 		},
 
 		izbranaPodnica(id) {
-            this.indeksIzbranePodnice = id
+      this.form.indeksIzbranePodnice = id
 			this.pridobiMeritve()
-        }
+		},
+		iskalniNiz() {
+			return "filter=podnica.id:EQ:" + this.form.indeksIzbranePodnice + " tip.koda:IN:[" + this.form.checked + "]"
+		},
+		search() {
+			this.offset = 0
+			this.meritve = []
+			this.pridobiMeritve()
+		}
 
-	}
+	},
+	created: function () {
+    fetch(global.restIp + "/tipi", {
+      method: "get"
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((jsonData) => {
+				const tipi = jsonData
+				for (const i in tipi) {
+					tipi[i].ime = this.pridobiImeMeritve(tipi[i].koda)
+				}
+				this.tipi = tipi
+      })
+  }
 
 
 }
